ab52cc7c10f943f508bb2dd46fee5d5fae8cd990
binary <msy90098@gmail.com>
2026-09-16 23:06:49 +0300

    initial commit

---
+++ test2.py
@@ -0,0 +1,32 @@
+dict={
+    "cybersecurity" : "my life",
+    "math": "i like it",
+    "networking" : "very good",
+    "linux" : "the main os"
+}
+
+print("search engine \n")
+while True:
+    userInput=input("enter the text : ").strip().lower()
+
+
+    if userInput=="exit":
+        break
+
+    valueFound = None
+
+    for key in dict:
+        if userInput == key:
+            valueFound=dict[key]
+            break
+        
+
+    if valueFound != None:
+        print(f"the result for that word you searched is : {valueFound}")
+    elif userInput == "quit":
+        print("system turn off")
+        break
+    else:
+        print(f"sorry not finding anythings - try again")
+
+    
