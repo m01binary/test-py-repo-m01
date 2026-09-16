f5641611d6874230d056055de2396a866b8da4c8
binary <msy90098@gmail.com>
2026-09-16 23:52:36 +0300

    new feature

--- test2.py
+++ test2.py
@@ -7,7 +7,7 @@ dict={
 
 print("search engine \n")
 while True:
-    userInput=input("enter the text : ").strip().lower()
+    userInput=input("enter the text : \nor type quit for exit").strip().lower()
 
 
     if userInput=="exit":
